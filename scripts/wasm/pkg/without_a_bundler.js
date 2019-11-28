fa0f1b20111f1e419949db47461b4f9552f34d10
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2019-11-28 10:48:05 -0500

    Added build artifacts to git

---
+++ scripts/wasm/pkg/without_a_bundler.js
@@ -0,0 +1,120 @@
+
+let wasm;
+
+let cachegetInt32Memory = null;
+function getInt32Memory() {
+    if (cachegetInt32Memory === null || cachegetInt32Memory.buffer !== wasm.memory.buffer) {
+        cachegetInt32Memory = new Int32Array(wasm.memory.buffer);
+    }
+    return cachegetInt32Memory;
+}
+
+let cachegetUint32Memory = null;
+function getUint32Memory() {
+    if (cachegetUint32Memory === null || cachegetUint32Memory.buffer !== wasm.memory.buffer) {
+        cachegetUint32Memory = new Uint32Array(wasm.memory.buffer);
+    }
+    return cachegetUint32Memory;
+}
+
+function getArrayU32FromWasm(ptr, len) {
+    return getUint32Memory().subarray(ptr / 4, ptr / 4 + len);
+}
+/**
+* @param {number} a
+* @param {number} b
+* @returns {Uint32Array}
+*/
+export function generate_range(a, b) {
+    const retptr = 8;
+    const ret = wasm.generate_range(retptr, a, b);
+    const memi32 = getInt32Memory();
+    const v0 = getArrayU32FromWasm(memi32[retptr / 4 + 0], memi32[retptr / 4 + 1]).slice();
+    wasm.__wbindgen_free(memi32[retptr / 4 + 0], memi32[retptr / 4 + 1] * 4);
+    return v0;
+}
+
+/**
+* @param {number} n
+* @returns {number}
+*/
+export function calculate_odds_of_same_birthday(n) {
+    const ret = wasm.calculate_odds_of_same_birthday(n);
+    return ret;
+}
+
+let cachegetUint16Memory = null;
+function getUint16Memory() {
+    if (cachegetUint16Memory === null || cachegetUint16Memory.buffer !== wasm.memory.buffer) {
+        cachegetUint16Memory = new Uint16Array(wasm.memory.buffer);
+    }
+    return cachegetUint16Memory;
+}
+
+let WASM_VECTOR_LEN = 0;
+
+function passArray16ToWasm(arg) {
+    const ptr = wasm.__wbindgen_malloc(arg.length * 2);
+    getUint16Memory().set(arg, ptr / 2);
+    WASM_VECTOR_LEN = arg.length;
+    return ptr;
+}
+/**
+* @param {Uint16Array} people
+* @returns {boolean}
+*/
+export function check_a_room_of_people(people) {
+    const ret = wasm.check_a_room_of_people(passArray16ToWasm(people), WASM_VECTOR_LEN);
+    return ret !== 0;
+}
+
+function init(module) {
+    if (typeof module === 'undefined') {
+        module = import.meta.url.replace(/\.js$/, '_bg.wasm');
+    }
+    let result;
+    const imports = {};
+
+    if ((typeof URL === 'function' && module instanceof URL) || typeof module === 'string' || (typeof Request === 'function' && module instanceof Request)) {
+
+        const response = fetch(module);
+        if (typeof WebAssembly.instantiateStreaming === 'function') {
+            result = WebAssembly.instantiateStreaming(response, imports)
+            .catch(e => {
+                return response
+                .then(r => {
+                    if (r.headers.get('Content-Type') != 'application/wasm') {
+                        console.warn("`WebAssembly.instantiateStreaming` failed because your server does not serve wasm with `application/wasm` MIME type. Falling back to `WebAssembly.instantiate` which is slower. Original error:\n", e);
+                        return r.arrayBuffer();
+                    } else {
+                        throw e;
+                    }
+                })
+                .then(bytes => WebAssembly.instantiate(bytes, imports));
+            });
+        } else {
+            result = response
+            .then(r => r.arrayBuffer())
+            .then(bytes => WebAssembly.instantiate(bytes, imports));
+        }
+    } else {
+
+        result = WebAssembly.instantiate(module, imports)
+        .then(result => {
+            if (result instanceof WebAssembly.Instance) {
+                return { instance: result, module };
+            } else {
+                return result;
+            }
+        });
+    }
+    return result.then(({instance, module}) => {
+        wasm = instance.exports;
+        init.__wbindgen_wasm_module = module;
+
+        return wasm;
+    });
+}
+
+export default init;
+
